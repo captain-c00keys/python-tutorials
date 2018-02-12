2b1ff70d9e5d08dbc039ecb3e058a39f01814c11
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-11 20:57:41 -0800

    added new lessons

---
+++ lesson3.py
@@ -0,0 +1,7 @@
+print(len("Manchester United"))
+
+print(str(12345) [2])
+
+print("albania".upper())
+
+print("BELGIUM".lower())
---
+++ newHelloWorld.py
@@ -0,0 +1,15 @@
+intVal = 5
+
+floatVal = 2.21
+
+boolVal = False
+
+print(boolVal)
+
+boolVal = True
+
+print(intVal)   
+
+print(floatVal)
+
+print(boolVal)
