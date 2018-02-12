e3152ded33c297607a22e7e946a603c44f0c0da1
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-11 22:08:20 -0800

    adding lecture 17

---
+++ lecture17.py
@@ -0,0 +1,4 @@
+print(7 > 6 and 6 >= 6) #true and true => true
+print(3 != 3 or 4 == 4) #false or true => true
+print(not 5 > 2) #false
+print(not 5 < 3 and true or 6 <= 6 and not false) #true and true or true and true => true
