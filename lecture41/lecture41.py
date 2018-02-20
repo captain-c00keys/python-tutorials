2a53a5e388271ad30990c65d6e1ca4529620027e
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-20 11:26:38 -0800

    ex for lecture 41

--- lecture41/lecture41.py
+++ lecture41/lecture41.py
@@ -1,3 +1,7 @@
 ex1 = [num1 + 1 for num1 in range(10)] #ex1 = [1,2,3,4,5,6,7,8,9,10]
 
 ex2 = [num2*3 for num2 in range(1,5)] #ex2 = [3,6,9,12]
+
+ex3 = [num1 for num1 in range(10) if num1 %2 == 0] #ex3 = [0,2,4,6,8]
+
+ex4 = [num2 for num2 in range(0, 99, 3) if num2 % 3 == 0 and num2 % 5 == 0] #ex4 = [0,15,30,45,60,75,90]
