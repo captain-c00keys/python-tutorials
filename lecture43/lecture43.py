160ae60d677b4a0f291c63cd777cc911ef96fbff
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-20 12:36:24 -0800

    setup lecture 43

---
+++ lecture43/lecture43.py
@@ -0,0 +1,15 @@
+exList = [1,2,3,4,5,6,7,8,9,10]
+
+sliced1 = exList[0:10:3] #sliced1 = [1,4,7,10]
+
+sliced2 = exlist[1:9:2] #sliced2 = [2,4,6,8]
+
+exList = [1,2,3,4,5]
+ommited = exList[::2] #omitted = [1,3,5]
+
+exList = [1,2,3,4,5,6,7,8,9,10]
+
+var1 = exList[8:1:-2] #var1 = [9,7,5,3]
+
+exList = [6,7,8,9,10]
+reversed = exList[::-1]
