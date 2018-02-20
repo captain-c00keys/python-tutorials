5e84bd278f8310e93610022d193b7eb1b6070474
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-20 11:42:27 -0800

    finished lecture 41

---
+++ lecture41/lecture41prac.py
@@ -0,0 +1,9 @@
+quest1 = [num1 for num1 in range(1,10,2)]
+    print(quest1)
+quest2 = [num2 for num2 in range(3,8)]
+    print(quest2)
+
+quest3 = [num3 for num3 in range(1,10) if num3 % 2 == 1]
+    print(quest3)
+quest4 = [num4 for num4 in range(8) if num4 > 2]
+    print(quest4)
