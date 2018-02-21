a3dc87c3c52f90324b88742ad197b8982403c078
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-20 16:15:08 -0800

    lecture 45

--- lecture45/lecture45prac.py
+++ lecture45/lecture45prac.py
@@ -4,3 +4,12 @@ print(rand)
 
 userNum = input("Enter an integer")
 
+try:
+    if rand > int(userNum):
+        print(rand)
+    elif int(userNum) > rand:
+        print(int(userNum))
+    else:
+        print("The number are the same")
+except:
+    print("Please run the program again and enter an integer")
